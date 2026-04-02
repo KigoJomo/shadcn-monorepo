7755a8902461dbd61a5680a129a3c31ba7df6322
KigoJomo <kigojomo@gmail.com>
2026-04-02 22:47:17 +0300

    feat: integrate Convex into layout and rebuild home page as tasks dashboard

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,14 @@
-import { Geist, Geist_Mono, Figtree, Roboto } from "next/font/google"
-
+import { Geist_Mono, Figtree, Roboto } from "next/font/google"
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@workspace/ui/lib/utils";
+import { cn } from "@workspace/ui/lib/utils"
+import { Toaster } from "@workspace/ui/components/sonner"
+import { TooltipProvider } from "@workspace/ui/components/tooltip"
+import { ConvexClientProvider } from "@/components/ConvexClientProvider"
 
-const robotoHeading = Roboto({subsets:['latin'],variable:'--font-heading'});
+const robotoHeading = Roboto({ subsets: ["latin"], variable: "--font-heading" })
 
-const figtree = Figtree({subsets:['latin'],variable:'--font-sans'})
+const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -22,10 +24,23 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", figtree.variable, robotoHeading.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        figtree.variable,
+        robotoHeading.variable
+      )}
     >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
+      <body className="h-dvh overflow-hidden relative">
+        <ConvexClientProvider>
+          <TooltipProvider>
+            <ThemeProvider>
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </TooltipProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,56 @@
-import { Button } from "@workspace/ui/components/button"
+import NewTask from "@/app/_components/new-task"
+import { api } from "@workspace/convex/api"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card"
+import { preloadQuery } from "convex/nextjs"
+import TasksTable from "./_components/tasks-table"
 
-export default function Page() {
+export default async function Home() {
+  const tasks = await preloadQuery(api.tasks.getAllTasks)
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="text-muted-foreground font-mono text-xs">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <>
+      <div className="fixed -top-48 -left-48 -z-5 aspect-square w-xl rounded-full bg-primary opacity-15 blur-3xl"></div>
+      <div className="fixed -right-48 -bottom-48 -z-5 aspect-square w-xl rounded-full bg-primary opacity-5 blur-3xl"></div>
+
+      <div className="fixed bottom-0 left-0 -z-10 grid h-[30vh] w-screen grid-cols-36 grid-rows-8">
+        {[...Array(36 * 8)].map((_, i) => (
+          <div key={i} className="-z-20 border"></div>
+        ))}
+
+        <div className="absolute -z-10 h-full w-full bg-linear-0 from-background/50 via-background/70 to-background/90"></div>
       </div>
-    </div>
+
+      <section className="mx-auto flex h-dvh max-w-4xl flex-col items-center gap-4 p-4 md:gap-8 md:p-12">
+        <div className="flex w-full items-center justify-between gap-4">
+          <h2>
+            Get <span className="italic">s#!+</span> done
+          </h2>
+        </div>
+
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>
+              <p className="font-medium">Hey dude :)</p>
+            </CardTitle>
+            <CardDescription>You&apos;ve got to lock in, mf!</CardDescription>
+
+            <CardAction>
+              <NewTask />
+            </CardAction>
+          </CardHeader>
+
+          <CardContent className="mt-4">
+            <TasksTable preloadedTasks={tasks} />
+          </CardContent>
+        </Card>
+      </section>
+    </>
   )
 }
